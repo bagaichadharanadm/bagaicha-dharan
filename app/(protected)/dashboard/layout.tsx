7201aa846931c4f47eca9ad1b6889d0e5389ef9c
Ishan Pradhan <ishanpradhan.ip@gmail.com>
2024-06-28 23:02:44 +0700

    style layout

--- app/(protected)/dashboard/layout.tsx
+++ app/(protected)/dashboard/layout.tsx
@@ -7,19 +7,19 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex flex-col h-screen bg-slate-50 text-slate-900">
+    <div className="flex flex-col h-screen bg-gray-100 text-gray-800">
       {/* Header */}
       <Header />
 
       {/* Main content */}
       <div className="flex flex-1 overflow-hidden">
         {/* Drawer */}
-        <div className="flex-shrink-0 lg:w-64 md:w-48 sm:w-32 w-20 overflow-y-auto scrollbar-hide border-r border-slate-200 bg-slate-100">
+        <div className="flex-shrink-0 lg:w-64 md:w-48 sm:w-32 w-20 overflow-y-auto scrollbar-hide border-r border-gray-300 bg-gray-200">
           <Drawer />
         </div>
 
         {/* Main content */}
-        <div className="flex-1 p-4 overflow-y-auto scrollbar-hide bg-slate-50">{children}</div>
+        <div className="flex-1 p-4 overflow-y-auto scrollbar-hide bg-gray-100">{children}</div>
       </div>
     </div>
   );
