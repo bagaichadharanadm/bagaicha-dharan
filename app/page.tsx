0614c3b887e39a90007334fd14d238a586056551
Ishan Pradhan <ishanpradhan.ip@gmail.com>
2024-06-28 17:59:17 +0700

    reset page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,5 @@
-import prisma from "@/db";
-
 export default async function Page() {
-  const testRecords = await prisma.test.findMany();
-  return (
-    <div>
-      {testRecords.map((rec) => {
-        return <div key={rec.id}>{rec.name}</div>;
-      })}
-    </div>
-  );
+  return <div></div>;
 }
 
 export const dynamic = "force-dynamic";
